161bdee4743125aabe37c35cd165c4859b7dea57
Piotr Bosowski <piotr.bosowski@gmail.com>
2024-06-30 13:16:25 +0200

    Implement players update_knowledge, moved public_know to Hanabi

--- src/player.cpp
+++ src/player.cpp
@@ -1,5 +1,18 @@
+#include <algorithm>
+
 #include "player.h"
 
 void Player::update_knowledge(
     const FuzzyCard &rejected_pool, const FuzzyCard &played_cards,
-    std::vector<std::vector<FuzzyCard>> other_players_hands) {}
+    std::vector<std::vector<FuzzyCard>> other_players_hands) {
+  private_knowledge = FuzzyCard(true);
+  for (auto players_hand : other_players_hands) {
+    private_knowledge += std::accumulate(players_hand.begin(),
+                                         players_hand.end(), FuzzyCard(true));
+  }
+}
+
+void Player::print() {
+  std::cout << std::endl << "Player's private knowledge:" << std::endl;
+  private_knowledge.print();
+}
--- src/player.h
+++ src/player.h
@@ -1,35 +1,25 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
-#include "fuzzycard.h"
+#include <iostream>
 #include <vector>
+
+#include "fuzzycard.h"
 
 class Player {
   /**
    * The knowledge available only to the Player - can never be shared!
    * Is comprised of:
    * - all other players' hands (PRIVATE),
-   * - rejected cards,
-   * - played cards.
    */
-  std::vector<FuzzyCard> private_knowledge;
-
-  /**
-   * The knowledge available to everyone - can always be shared.
-   * Is comprised of:
-   * - rejected cards,
-   * - played cards.
-   */
-  std::vector<FuzzyCard> public_knowledge;
-
-  // actual hand of cards - not visible to the player in Hanabi
-  std::vector<FuzzyCard> hand;
+  FuzzyCard private_knowledge;
 
 public:
   void
   update_knowledge(const FuzzyCard &rejected_pool,
                    const FuzzyCard &played_cards,
                    std::vector<std::vector<FuzzyCard>> other_players_hands);
+  void print();
 };
 
 #endif
